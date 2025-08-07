e296fbe94d3427a61d37e62e15e93e1edd769b57
2concrete <danielpeace654@gmail.com>
2025-08-07 17:45:58 +0800

    just redoing everything

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import HomePage from "./pages/HomePage.jsx";
-import ProjectsPage from "./pages/ProjectsPage.jsx";
-import ContactPage from "./pages/ContactPage.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/projects", element: <ProjectsPage /> },
-  { path: "/contact", element: <ContactPage /> },
-]);
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
