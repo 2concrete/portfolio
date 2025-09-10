ee8c2a93eb071245f3ec16061bbe163d8c8ca216
2concrete <danielpeace654@gmail.com>
2025-09-10 10:00:27 +0800

    hotfix

--- src/components/RandomCat.jsx
+++ src/components/RandomCat.jsx
@@ -3,12 +3,14 @@ import { FiRefreshCcw } from "react-icons/fi";
 import { motion } from "framer-motion";
 
 const RandomCat = () => {
+  const api_key = import.meta.env.VITE_API_KEY;
+
   const [image, setImage] = useState({});
   const [rotation, setRotation] = useState(0);
 
   const getRandomImage = async () => {
     const response = await fetch(
-      "https://api.thecatapi.com/v1/images/search?api_key=api_keylive_5KTdnuXxmukJo7KjjHAOmxltfVKURURsJDF4CXa3FAAGgRaxCjT5hOm57o786vUt"
+      `https://api.thecatapi.com/v1/images/search?api_key=${api_key}`
     );
     const data = await response.json();
     setImage(data[0]);
