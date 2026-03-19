7b902b421ab77e656c4701c0deb43fee2ecdc23e
2concrete <danielpeace654@gmail.com>
2026-03-19 15:06:53 +0800

    added some projects and polished loading with cat image

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -6,24 +6,40 @@ const Projects = () => {
       title: "minimalist",
       description: "hyper minimal todolist",
       tags: ["nextjs", "react", "tailwindcss", "convexDB", "clerk"],
+      github: "https://github.com/2concrete/minimalist",
+      link: "https://minimalist.ws",
+    },
+    {
+      title: "wrd",
+      description: "random website to display text",
+      tags: ["nextjs", "react", "tailwindcss", "convexDB"],
+      github: "https://github.com/2concrete/wrd",
+      link: "https://wrd-hazel.vercel.app/",
+    },
+    {
+      title: "portfolio",
+      description: "this minimal portfolio",
+      tags: ["nextjs", "react", "tailwindcss"],
+      github: "https://github.com/2concrete/portfolio",
+      link: "https://concrete.codes",
     },
   ];
 
   return (
-    <div className="mb-10">
+    <div className="">
       <p className="text-2xl mb-5">projects</p>
       {projects.map((project) => {
         return (
-          <div key={project.title} className="flex justify-between">
+          <div key={project.title} className="flex justify-between mb-4">
             <div className="flex-col flex">
               <span className="flex items-center gap-2">
                 <a
-                  href="https://minimalist.ws"
+                  href={project.link}
                   className="text-lg hover:opacity-70 transition-all"
                 >
                   {project.title}
                 </a>
-                <a href="https://github.com/2concrete/minimalist-v2">
+                <a href={project.github}>
                   <FaGithub className="size-4 cursor-pointer hover:opacity-70 transition-all" />
                 </a>
               </span>
--- app/components/RandomCat.tsx
+++ app/components/RandomCat.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import { AnimatePresence, motion } from "motion/react";
 import { useEffect, useState } from "react";
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const RandomCat = () => {
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
@@ -18,38 +17,65 @@ const RandomCat = () => {
       const response = await fetch(`${apiUrl}?api_key=${apiKey}`);
       const data = await response.json();
       setImageUrl(data[0].url);
-      setLoadingImage(false);
+      setTimeout(() => {
+        setLoadingImage(false);
+      }, 1000);
     };
 
     getImage();
   }, [refresh, apiKey, apiUrl]);
+
+  useEffect(() => {
+    setImageLoaded(false);
+  }, [imageUrl]);
 
   if (imageUrl) {
     return (
       <div>
         {imageUrl && (
           <>
-            <div className="relative">
-              <Image
-                loading="eager"
-                src={imageUrl}
-                alt="Image of cat"
-                onLoad={() => setImageLoaded(true)}
-                width={300}
-                height={300}
-                preload={true}
-                style={{
-                  width: 250,
-                  height: 250,
-                  objectFit: "cover",
-                  borderRadius: 3,
-                }}
-              />
-              {loadingImage && (
-                <AiOutlineLoading3Quarters
-                  className={`${loadingImage && "animate-spin"} absolute bottom-2 left-2`}
-                />
-              )}
+            <div className="relative flex justify-center items-center border border-neutral-900 rounded-lg">
+              <AnimatePresence mode="wait">
+                <motion.div
+                  key={imageUrl}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: imageLoaded ? 1 : 0 }} // ← Only fade in when loaded
+                  transition={{ duration: 0.3 }}
+                  exit={{ opacity: 0 }}
+                >
+                  <Image
+                    loading="eager"
+                    src={imageUrl}
+                    alt="Image of cat"
+                    onLoad={() => setImageLoaded(true)}
+                    width={300}
+                    height={300}
+                    preload={true}
+                    unoptimized
+                    style={{
+                      width: 250,
+                      height: 250,
+                      objectFit: "cover",
+                      borderRadius: 3,
+                    }}
+                  />
+                </motion.div>
+              </AnimatePresence>
+              <AnimatePresence mode="wait">
+                {loadingImage && (
+                  <motion.div
+                    key="loading-spinner"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5 }}
+                    exit={{ opacity: 0 }}
+                    className="absolute flex justify-center items-center"
+                  >
+                    <div className="rounded-full w-10 h-10 border-2 border-transparent border-t-neutral-700 border-r-neutral-700 border-b-neutral-700 animate-spin" />
+                  </motion.div>
+                )}
+              </AnimatePresence>
+
               <AnimatePresence>
                 {imageLoaded && (
                   <motion.button
@@ -59,7 +85,7 @@ const RandomCat = () => {
                     transition={{ duration: 0.3 }}
                     exit={{ opacity: 0 }}
                     whileHover={{ opacity: 0.7 }}
-                    className="cursor-pointer flex items-center gap-2 absolute right-0"
+                    className="cursor-pointer flex items-center gap-2 absolute right-0 -bottom-6"
                     type="button"
                     onClick={() => {
                       setRefresh(!refresh);
