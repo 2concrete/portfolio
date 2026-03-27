9167aa757f501c37f5232b96381a25db818c88ac
2concrete <danielpeace654@gmail.com>
2026-03-27 13:42:42 +0800

    added margin at bottom of site

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Top from "./components/Top";
 export default function Home() {
   return (
     <>
-      <main className="lg:w-2xl md:w-2xl sm:w-xl w-sm mx-auto mt-16 font-[Inter] flex flex-col gap-20">
+      <main className="mb-15 lg:w-2xl md:w-2xl sm:w-xl w-sm mx-auto mt-16 font-[Inter] flex flex-col gap-20">
         <section className="flex justify-between">
           <Top />
           <RandomCat />
