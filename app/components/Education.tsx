774e418690b1c515d4cb86a81fb4b622f1bb13f7
2concrete <danielpeace654@gmail.com>
2026-05-01 18:58:41 +0800

    removed curtin

--- app/components/Education.tsx
+++ app/components/Education.tsx
@@ -4,13 +4,6 @@ const Education = () => {
       <p className="text-2xl mb-5">education</p>
       <div className="flex flex-col gap-4">
         <span>
-          <p className="text-lg">curtin university</p>
-          <p className="text-sm opacity-80">
-            bachelor of information technology
-          </p>
-          <p className="text-xs opacity-50">2026-2028</p>
-        </span>
-        <span>
           <p className="text-lg">kolbe catholic college</p>
           <p className="text-sm opacity-80">
             academic excellence award <br />
