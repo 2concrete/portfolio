db2a910543ed4febabe511dec225f36b6e428306
2concrete <danielpeace654@gmail.com>
2026-05-04 12:26:06 +0800

    added link to tailscale server

--- app/components/Top.tsx
+++ app/components/Top.tsx
@@ -1,13 +1,22 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import Link from "next/link";
 import SocialLink from "./SocialLink";
+import { IoStar } from "react-icons/io5";
 
 const Top = () => {
   return (
     <>
       <div className="flex flex-col gap-2 justify-between">
         <div>
-          <h1 className="text-4xl mb-1.5">hi, i&apos;m daniel</h1>
+          <span className="flex gap-2 items-center relative">
+            <h1 className="text-4xl mb-1.5">hi, i&apos;m daniel</h1>
+            <Link
+              className="relative bottom-0.5 hover:opacity-60 transition-all duration-400"
+              href={"https://concrete.seagull-little.ts.net/"}
+            >
+              <IoStar />
+            </Link>
+          </span>
           <p>web developer</p>
           <p className="text-sm opacity-70">perth, australia</p>
         </div>
