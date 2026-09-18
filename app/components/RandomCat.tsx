dee11cbd5c75bdd4d901a3cc9b9264224d7c2ba2
2concrete <danielpeace654@gmail.com>
2026-09-18 21:44:40 +0800

    fixed RandomCat.tsx

--- app/components/RandomCat.tsx
+++ app/components/RandomCat.tsx
@@ -47,85 +47,78 @@ const RandomCat = () => {
   }, [imageUrl]);
 
   useEffect(() => {
-    setImageLoaded(Boolean(imageUrl));
+    setImageLoaded(false);
     setIsRefreshing(false);
   }, [imageUrl]);
 
   return (
     <div className="relative flex justify-center items-center border border-neutral-900 rounded-lg w-62.5 h-62.5 shrink-0">
-      {imageUrl ? (
-        <>
-          <AnimatePresence mode="wait">
-            <motion.div
-              key={imageUrl}
+      <>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={imageUrl}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: imageLoaded ? 1 : 0 }}
+            transition={{ duration: 0.3 }}
+            exit={{ opacity: 0 }}
+            className="relative z-10"
+          >
+            <Image
+              loading="eager"
+              src={imageUrl ? imageUrl : "idk"}
+              alt="Image of cat"
+              onLoad={() => setImageLoaded(true)}
+              width={300}
+              height={300}
+              preload={true}
+              unoptimized
+              style={{
+                width: "250px",
+                height: "250px",
+                objectFit: "cover",
+                borderRadius: 3,
+              }}
+            />
+          </motion.div>
+        </AnimatePresence>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key="loading-spinner"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            exit={{ opacity: 0 }}
+            className="absolute inset-0 z-0 flex items-center justify-center"
+          >
+            <div className="rounded-full w-10 h-10 border-2 border-transparent border-t-neutral-700 border-r-neutral-700 border-b-neutral-700 animate-spin" />
+          </motion.div>
+        </AnimatePresence>
+
+        <AnimatePresence>
+          {imageLoaded && !isRefreshing && (
+            <motion.button
+              key="refresh-button"
               initial={{ opacity: 0 }}
-              animate={{ opacity: imageLoaded ? 1 : 0 }}
+              animate={{ opacity: 1 }}
               transition={{ duration: 0.3 }}
               exit={{ opacity: 0 }}
-              className="relative z-10"
+              whileHover={{ opacity: 0.7 }}
+              className="cursor-pointer flex items-center gap-2 absolute -bottom-6 right-0 z-10"
+              type="button"
+              onClick={() => {
+                setImageLoaded(false);
+                setIsRefreshing(true);
+                window.localStorage.removeItem(CAT_STORAGE_KEY);
+                setTimeout(() => {
+                  setImageUrl("");
+                }, 300);
+              }}
             >
-              <Image
-                loading="eager"
-                src={imageUrl}
-                alt="Image of cat"
-                onLoad={() => setImageLoaded(true)}
-                width={300}
-                height={300}
-                preload={true}
-                unoptimized
-                style={{
-                  width: "250px",
-                  height: "250px",
-                  objectFit: "cover",
-                  borderRadius: 3,
-                }}
-              />
-            </motion.div>
-          </AnimatePresence>
-          <AnimatePresence mode="wait">
-            {loadingImage && (
-              <motion.div
-                key="loading-spinner"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5 }}
-                exit={{ opacity: 0 }}
-                className="absolute inset-0 z-0 flex items-center justify-center"
-              >
-                <div className="rounded-full w-10 h-10 border-2 border-transparent border-t-neutral-700 border-r-neutral-700 border-b-neutral-700 animate-spin" />
-              </motion.div>
-            )}
-          </AnimatePresence>
-
-          <AnimatePresence>
-            {imageLoaded && !isRefreshing && (
-              <motion.button
-                key="refresh-button"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.3 }}
-                exit={{ opacity: 0 }}
-                whileHover={{ opacity: 0.7 }}
-                className="cursor-pointer flex items-center gap-2 absolute -bottom-6 right-0 z-10"
-                type="button"
-                onClick={() => {
-                  setImageLoaded(false);
-                  setIsRefreshing(true);
-                  window.localStorage.removeItem(CAT_STORAGE_KEY);
-                  setTimeout(() => {
-                    setImageUrl("");
-                    setRefresh(!refresh);
-                  }, 300);
-                }}
-              >
-                click me
-              </motion.button>
-            )}
-          </AnimatePresence>
-        </>
-      ) : (
-        <div className="h-62.5 w-62.5" />
-      )}
+              click me
+            </motion.button>
+          )}
+        </AnimatePresence>
+      </>
     </div>
   );
 };
