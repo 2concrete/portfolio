7cf2b00486cb9a1f6ac26cb66d38464dacfbc620
2concrete <danielpeace654@gmail.com>
2026-09-19 13:32:55 +0800

    added resume at /resume

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://cdn2.thecatapi.com/**")],
   },
+  async rewrites() {
+    return [{ source: "/resume", destination: "/resume/index.html" }];
+  },
 };
 
 export default nextConfig;
